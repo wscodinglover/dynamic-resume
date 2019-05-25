2cafcc346bf9dcae0fb74ba1f9097ffa10d50997
wangshuai <shuai.wang@dadaabc.com>
2019-05-25 21:45:15 +0800

    动态简历

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path'),
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+
+    args = process.argv,
+    debug = args.indexOf('--debug') > -1,
+    output = {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[hash].js'
+    },
+    plugins = [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            hash: true,
+            template: path.resolve(__dirname, './lib/index.tpl.html')
+        }),
+        new ExtractTextPlugin('[name].[hash].css')
+    ];
+
+if (!debug) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin())
+}
+
+module.exports = {
+    context: path.resolve(__dirname, './lib'),
+    entry: {
+        app: './app.js'
+    },
+    output: output,
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: [/node_modules/],
+            use: [{
+                loader: 'babel-loader'
+            }]
+        }, {
+            test: /\.(scss|css)$/,
+            use: ExtractTextPlugin.extract({
+                use: ['css-loader', 'sass-loader'],
+                fallback: 'style-loader'
+            })
+        }]
+    },
+    devServer: {
+        host: '127.0.0.1',
+        contentBase: path.resolve(__dirname, './dist'),
+        historyApiFallback: true,
+        open: true
+    },
+    plugins: plugins
+}
